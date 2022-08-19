3ceeedd9c6d4dab7517f84914fbff07f5727fb90
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-19 17:34:27 -0300

    feat: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/helpers/http/http-helper.ts
+++ src/presentation/helpers/http/http-helper.ts
@@ -1,15 +1,20 @@
-import { ServerError, UnauthorizedError } from '../../errors';
-import { HttpResponse } from '../../protocols/http';
+import { ServerError, UnauthorizedError } from '../../errors'
+import { HttpResponse } from '../../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
-}) 
+})
 
 export const unauthorized = (): HttpResponse => ({
   statusCode: 401,
   body: new UnauthorizedError()
-}) 
+})
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
 
 export const serverError = (error: Error): HttpResponse => ({
   statusCode: 500,
