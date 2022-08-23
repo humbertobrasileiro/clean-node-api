83d0f4ae2ad82f83967b701e143506df7b7af5ae
Carlos Humberto Brasileiro Toscano <48128007+humbertobrasileiro@users.noreply.github.com>
2022-08-23 15:55:45 -0300

    test: ensure AddSurveyController returns a badRequest error if validation fails

--- tests/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ tests/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,5 +1,6 @@
 import { HttpRequest, Validation } from '../../../../../src/presentation/controllers/survey/add-survey/add-survey-controller-protocols'
 import { AddSurveyController } from '../../../../../src/presentation/controllers/survey/add-survey/add-survey-controller'
+import { badRequest } from '../../../../../src/presentation/helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -34,7 +35,6 @@ const makeSut = (): SutTypes => {
   }
 }
 
-
 describe('AddSurvey Controller', () => {
 
   test('Should call Validation with correct values', async () => {
@@ -43,6 +43,13 @@ describe('AddSurvey Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 
 })
